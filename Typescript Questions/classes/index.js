df274c7c55701df94d44aba939cf762542f5a58a
Sherazhafiz <sherazhafiz751@gmail.com>
2023-10-01 11:14:31 +0500

    parameterised constructor added

--- Typescript Questions/classes/index.js
+++ Typescript Questions/classes/index.js
@@ -1,21 +1,27 @@
 "use strict";
 class car {
-    constructor() {
+    // constructor(){
+    // this.model="toyota";
+    // this.year=2023;
+    // this.price=3000000;
+    // this.color="Red"
+    // }
+    constructor(model, year, price, color) {
         this.model = "";
         this.year = 0;
         this.price = 0;
         this.color = '';
-        this.model = "toyota";
-        this.year = 2023;
-        this.price = 3000000;
-        this.color = "Red";
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
     }
 }
-let car1 = new car();
-let car2 = new car();
-car1.model = "toyota";
-car1.year = 2022;
-car1.color = "Black";
-car1.price = 2000000;
+let car1 = new car("toyota", 2023, 3000000, "Red");
+// let car2:cartype=new car();
+// car2.model="toyota";
+// car2.year=2022;
+// car2.color="Black";
+// car2.price=2000000;
+// console.log(car1);
 console.log(car1);
-console.log(car2);
--- Typescript Questions/classes/index.ts
+++ Typescript Questions/classes/index.ts
@@ -5,24 +5,30 @@ class car{
     price:number=0;
     color:string=''
 
-    constructor(){
-    this.model="toyota";
-    this.year=2023;
-    this.price=3000000;
-    this.color="Red"
+    // constructor(){
+    // this.model="toyota";
+    // this.year=2023;
+    // this.price=3000000;
+    // this.color="Red"
+    // }
+    constructor(model:string, year:number, price:number,color:string){
+    this.model=model;
+    this.year=year;
+    this.price=price;
+    this.color=color;
     }
 }
 
-let car1:cartype=new car();
-let car2:cartype=new car();
-car1.model="toyota";
-car1.year=2022;
-car1.color="Black";
-car1.price=2000000;
+let car1:cartype=new car("toyota",2023,3000000,"Red");
+// let car2:cartype=new car();
+// car2.model="toyota";
+// car2.year=2022;
+// car2.color="Black";
+// car2.price=2000000;
 
 
+// console.log(car1);
 console.log(car1);
-console.log(car2);
 
 
 
